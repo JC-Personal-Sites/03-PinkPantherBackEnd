23c69058df60a786285784681887e75eb004cf8a
Justin Heath <just.coder@outlook.com>
2024-02-05 14:39:42 +0000

    refactor: add delay to 2 enpoints for display simulations

--- app/_routes/NavBar/NavBar-Controller.js
+++ app/_routes/NavBar/NavBar-Controller.js
@@ -5,6 +5,8 @@ const asyncHandler = require('../../middleware/async');
 exports.getNavBars = asyncHandler(async (req, res, next) => {
 	try {
 		const navBars = await NavBarSchema.find().sort({ 'id': 'asc' });
+		// REMOVE - This is just added to show suspense working
+		await new Promise(resolve => setTimeout(resolve, 1000))
 
 		res.status(200).json({ data: navBars });
 	} catch (err) {
--- app/_routes/Socials/Socials-Controller.js
+++ app/_routes/Socials/Socials-Controller.js
@@ -5,6 +5,8 @@ const asyncHandler = require('../../middleware/async');
 exports.getSocials = asyncHandler(async (req, res, next) => {
 	try {
 		const socials = await SocialsSchema.find();
+		// REMOVE - This is just added to show suspense working
+		await new Promise(resolve => setTimeout(resolve, 2000))
 
 		res.status(200).json({ data: socials });
 	} catch (err) {
