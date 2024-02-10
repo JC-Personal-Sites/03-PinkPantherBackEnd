bc049325ca0dfbb5a7d3860b8a28b9695244d4de
Justin Heath <70263625+justcoder1@users.noreply.github.com>
2024-02-10 21:15:14 +0000

    refactor: http addresses updated (#14)

---
+++ app/_routes/PinkPanther/NavBar/NavBar-Controller.js
@@ -0,0 +1,13 @@
+const NavBarSchema = require('./NavBar-Model');
+const ErrorResponse = require('../../../middleware/errorResponse');
+const asyncHandler = require('../../../middleware/async');
+
+exports.getNavBars = asyncHandler(async (req, res, next) => {
+  try {
+    const navBars = await NavBarSchema.find().sort({ id: 'asc' });
+
+    res.status(200).json({ data: navBars });
+  } catch (err) {
+    return next(new ErrorResponse(`Data not found`, 500));
+  }
+});
--- app/_routes/_Root/Root-Controller.js
+++ app/_routes/_Root/Root-Controller.js
@@ -2,9 +2,9 @@ const ErrorResponse = require('../../middleware/errorResponse');
 const asyncHandler = require('../../middleware/async');
 
 exports.getRoot = asyncHandler(async (req, res, next) => {
-	try {
-		res.status(200).json({ data: {hi: 'Hi Justin How Are you?'} });
-	} catch (err) {
-		return next(new ErrorResponse(`Data not found`, 500));
-	}
+  try {
+    res.status(200).json({ data: { hi: 'Hi Justin How Are you?' } });
+  } catch (err) {
+    return next(new ErrorResponse(`Data not found`, 500));
+  }
 });
