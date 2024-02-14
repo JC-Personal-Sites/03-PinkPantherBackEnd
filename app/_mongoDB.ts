7f78b5de39b04e67a1300e1e23115f6586e3d758
Justin Heath <just.coder@outlook.com>
2024-02-14 13:48:52 +0000

    fix: typeing error corrected

--- app/_mongoDB.ts
+++ app/_mongoDB.ts
@@ -3,7 +3,7 @@ import mongoose, { connect } from "mongoose";
 const connectDB = async () => {
   mongoose.Promise = global.Promise;
 
-  const conn = await connect(process.env.MONGODB_URI, {});
+  const conn = await connect(process.env.MONGODB_URI || "", {});
 
   console.log(`MongoDB Conected ${conn.connection.name}`.cyan.underline.bold);
 };
