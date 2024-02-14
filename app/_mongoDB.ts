e853496a5b540f33d3e7723ca30c6d6a93129be9
Justin Heath <just.coder@outlook.com>
2024-02-14 13:52:22 +0000

    fix: typeing error corrected

--- app/_mongoDB.ts
+++ app/_mongoDB.ts
@@ -4,11 +4,7 @@ const connectDB = async (): Promise<void> => {
   try {
     mongoose.Promise = global.Promise;
 
-<<<<<<< HEAD
-  const conn = await connect(process.env.MONGODB_URI || "", {});
-=======
-    const conn = await connect(process.env.MONGODB_URI, {});
->>>>>>> 98b7631dae0d9b02983bfdc7e9474d32e140d3fc
+    const conn = await connect(process.env.MONGODB_URI || "", {});
 
     console.log(`MongoDB Conected ${conn.connection.name}`.cyan.underline.bold);
   } catch (err) {
