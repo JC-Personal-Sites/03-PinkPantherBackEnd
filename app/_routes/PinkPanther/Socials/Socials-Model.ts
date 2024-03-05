585ec743c1af36d4e66b48bf7079a3635325b133
Justin Heath <just.coder@outlook.com>
2024-03-05 21:09:39 +0000

    refactor: added slug middleware

--- app/_routes/PinkPanther/Socials/Socials-Model.ts
+++ app/_routes/PinkPanther/Socials/Socials-Model.ts
@@ -1,15 +1,22 @@
 import { Schema, model } from "mongoose";
+import slugify from "slugify";
 
 const SocialsSchema = new Schema(
   {
-    id: { type: Number, unique: true, required: [true, "Required"] },
-    social: { type: String, unique: true, required: [true, "Required"] },
-    icon: { type: String, unique: true, required: [true, "Required"] },
-    link: { type: String, unique: true, required: [true, "Required"] },
+    id: { type: Number, unique: true, required: [true, "id is Missing"] },
+    slug: { type: String },
+    social: { type: String, unique: true, required: [true, "social is Missing"] },
+    icon: { type: String, unique: true, required: [true, "icon is Missing"] },
+    link: { type: String, unique: true, required: [true, "link is Missing"] },
   },
   {
     versionKey: false,
   }
 );
+
+SocialsSchema.pre("save", function (next) {
+  this.slug = slugify(`${this.id} ${this.social}`, { lower: true });
+  next();
+});
 
 export default model("Pinkpanthersocials", SocialsSchema);
--- app/_routes/PinkPanther/Users/Users-Model.ts
+++ app/_routes/PinkPanther/Users/Users-Model.ts
@@ -1,11 +1,14 @@
 import { Schema, model } from "mongoose";
+import slugify from "slugify";
 
 const UserSchema = new Schema(
   {
-    firstName: { type: String, required: [true, "Required"] },
-    lastName: { type: String, required: [true, "Required"] },
+    id: { type: Number, unique: true, required: [true, "id is Missing"] },
+    slug: { type: String },
+    firstName: { type: String, required: [true, "firstName is Missing"] },
+    lastName: { type: String, required: [true, "lastName is Missing"] },
     phoneNumber: { type: String },
-    emailAddress: { type: String, unique: true, required: [true, "Required"] },
+    emailAddress: { type: String, unique: true, required: [true, "emailAddress is Missing"] },
     job: { type: String },
     type: { type: String },
   },
@@ -13,5 +16,10 @@ const UserSchema = new Schema(
     versionKey: false,
   }
 );
+
+UserSchema.pre("save", function (next) {
+  this.slug = slugify(`${this.id} ${this.firstName} ${this.lastName}`, { lower: true });
+  next();
+});
 
 export default model("Pinkpantherusers", UserSchema);
