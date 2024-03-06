c9d1b20617b8b0cc4a1addc6dc4bd2e03890ba05
Justin Heath <just.coder@outlook.com>
2024-03-06 16:16:18 +0000

    refactor: authentication login started

--- app/_routes/PinkPanther/Authentication/Authentication-Routes.ts
+++ app/_routes/PinkPanther/Authentication/Authentication-Routes.ts
@@ -1,16 +1,22 @@
 import express from "express";
+import rateLimit from "express-rate-limit";
 import {
-  registerAuth,
-  loginAuth,
-  forgotPasswordAuth,
   approveResetAuth,
+  forgotPasswordAuth,
+  loginAuth,
+  registerAuth,
   resetPasswordAuth,
 } from "./Authentication-Controller";
+
+const apiLimited = rateLimit({
+  windowMs: 15000, // 15 minutes
+  max: 6,
+});
 
 const authenticationRoute = express
   .Router()
   .post("/register", registerAuth)
-  .post("/login", loginAuth)
+  .post("/login", apiLimited, loginAuth)
   .post("/forgotPassword", forgotPasswordAuth)
   .post("/approveReset", approveResetAuth)
   .post("/resetPassword", resetPasswordAuth);
