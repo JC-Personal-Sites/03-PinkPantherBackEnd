51bae58323d3941c58bc5d5a9a2d61d3fd9d4a32
Justin Heath <just.coder@outlook.com>
2024-03-07 11:01:07 +0000

    refactor: authentication control user access to navbar

--- app/_routes/PinkPanther/NavBar/NavBar-Controller.ts
+++ app/_routes/PinkPanther/NavBar/NavBar-Controller.ts
@@ -1,11 +1,19 @@
-import { type NextFunction, type Request, type Response } from "express";
+import { type NextFunction, type Response } from "express";
 import asyncHandler from "express-async-handler"; // See notes in _Root
 
-import NavBarSchema from "./NavBar-Model";
+import type { I_RequestUser } from "../_localHelpers/modelHelper";
 
-const getNavBars = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
+import NavBarSchema from "./NavBar-Model";
+import RoleSchema from "../Roles/Roles-Model";
+
+const getNavBars = asyncHandler(async (req: I_RequestUser, res: Response, next: NextFunction) => {
+  const userAllowedTabs = await RoleSchema.findOne({ _id: req.user.roleId });
   const navBars = await NavBarSchema.find().sort({ id: "asc" });
-  res.status(200).json({ data: navBars });
+
+  // reduce the navbar tabs by userRole
+  // @ts-expect-error
+  const allowedTabs = navBars.filter(({ _id }) => userAllowedTabs.navBarIds.includes(_id));
+  res.status(200).json({ data: allowedTabs });
 });
 
 export default getNavBars;
